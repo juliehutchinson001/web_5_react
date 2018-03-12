7dca6490f2a54a48da07174d85416978a0718d2d
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-12 16:49:46 -0700

    feat: implement return for App layout

--- react_lab_1/product-list-app/src/App.js
+++ react_lab_1/product-list-app/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+import SortingBar from './components/sortingBar';
+//ProductGrid is the inventory component where items are shown
 import ProductGrid from './components/productGrid';
+//inventory and category are extracted from product.js
+//inventory is an array of items
+//categories are the sorted items in the array
 import inventory, {categories} from './products';
 import './App.css';
 
-console.log(categories);
-console.log(products);
-
+//This is the outward layer where the inner components are laid out
 class App extends Component {
     render() {
-        return ( <div className = "App" >
-                    <ProductGrid products={products} categories={categories} />
-                </div>
+        return ( 
+            <div className = "App" >
+                <SortingBar categories={categories}/>
+                <ProductGrid products={inventory} categories={categories} />
+            </div>
         );
     }
 }
+
+
+
 
 export default App;
