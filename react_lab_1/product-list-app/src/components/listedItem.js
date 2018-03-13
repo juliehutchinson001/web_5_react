becd2634e0d1462f5a73aa3fb19274734b7bf8ac
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-12 17:12:02 -0700

    feat: defined ListedItem component

--- react_lab_1/product-list-app/src/components/listedItem.js
+++ react_lab_1/product-list-app/src/components/listedItem.js
@@ -1,1 +1,16 @@
 import React, { Component } from 'react';
+
+class ListedItem extends Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.name}</h1>
+                <p>{this.props.price}</p>
+                <p>{this.props.category}</p>
+
+            </div>
+        )
+    };
+}
+
+export default ListedItem
