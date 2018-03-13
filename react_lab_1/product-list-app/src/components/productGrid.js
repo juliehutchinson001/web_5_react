982e713315e0c3de31ab20319f90328927c3cf75
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-12 17:13:35 -0700

    feat: definition of productGrid component

--- react_lab_1/product-list-app/src/components/productGrid.js
+++ react_lab_1/product-list-app/src/components/productGrid.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
-import inventory from './products';
+import ListedItem from './listedItem';
 
-class productGrid extends Component {
+class ProductGrid extends Component {
+
+    namedItems() {
+        return this.props.products.map((product) => {
+            return <ListedItem 
+                key={product.id}
+                className="Item"
+                name={product.name}
+                price={product.price} 
+                category={product.category} />
+
+        })
+    }
+
+    categories() {
+        return this.props.category.map((classification) => {
+            
+        })
+    }
+
     render() {
         return ( <div>
-                    <ListedItem products={products} categories={categories} />
+                    {/* display categories here */}
+                    {
+                        this.namedItems()
+                    }
                 </div>
+        )
     }
-}
+};
+
+export default ProductGrid
+
+// var a = [1,2,3,4]
+
+// var b = a.map((price) => {
+//     return "$"+price
+// })
+
+// ["$1", "$2", ]
