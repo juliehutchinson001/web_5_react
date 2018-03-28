3046cc25c16ec0c2a3bac8a96d807c75bbab5f95
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-28 16:18:15 -0700

    feat: defining components to create weather app

--- awesome_project/App.js
+++ awesome_project/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, ActivityIndicator  } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.welcome}>
+          San Francisco{'\n'}
+          Sunday, October 1
+        </Text>
+        <Text style={styles.temperature}>
+          57 degrees
+        </Text>
+        <Text style={styles.temperature}>
+          Partly Cloudy
+        </Text>
+        <Text style={styles.temperature}>
+          Humidity 78%{'\n'}
+        </Text>
+        <Text style={styles.temperature}>
+          Wind Moderate
+        </Text>
+        <Text style={styles.temperature}>
+          Mon Tues Wed Thur Fri Sat Sun{'\n'}
+           67  70  69   68  75  74   73
+        </Text>
+        <Text style={styles.temperature}>
+          Mon Tues Wed Thur Fri Sat Sun{'\n'}
+           67  70  69   68  75  74   73
+        </Text>
       </View>
     );
   }
@@ -16,8 +37,18 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
